5fb58db6149364a72907d094213c0f4d21b78476
Umer Dawood <145896009+umerdawood23@users.noreply.github.com>
2026-05-26 23:38:16 +0500

    Creating Variables

---
+++ Phase 7_Python_Basic_40_Coding_Problems/1) Exercise 1. Arithmetic Product and Conditional Logic.py
@@ -0,0 +1,23 @@
+#Practice Problem: Write a Python function that accepts two integer numbers. If the product of the two numbers is less than or equal to 1000, return their product; otherwise, return their sum.
+
+item = "Laptop"
+price = 1250
+quantity = 2
+total = price * quantity
+
+print("You bought {} {}s for a total of ${}.".format(quantity, item, total))
+
+
+first_name = "umer" #(Keep it lowercase)
+last_name = "dawood" #(Keep it lowercase)
+year = 2026
+
+print("Welcome, {} {}! Your generated email is umer.dawood2026@company.com".format(first_name.title(), last_name.title(), year))
+
+player1 = "Alice"
+score1 = 95
+player2 = "Bob"
+score2 = 100
+
+print("PLAYER: {} | SCORE: {}".format(player1, score1))
+print("PLAYER: {} | SCORE: {}".format(player2, score2))
