34d00ebee1ce977fb8a979988da605e8dd369d8e
Umer Dawood <145896009+umerdawood23@users.noreply.github.com>
2026-06-01 23:08:34 +0500

    2) Strings_are_Arrays.py

--- Python_Syntax/2) Strings_are_Arrays.py
+++ Python_Syntax/2) Strings_are_Arrays.py
@@ -1,9 +1,4 @@
-"""Strings are Arrays
-Like many other popular programming languages, strings in Python are arrays of unicode characters.
 
-However, Python does not have a character data type, a single character is simply a string with a length of 1.
-
-Square brackets can be used to access elements of the string."""
 
 
 a = "Hello, World!"
