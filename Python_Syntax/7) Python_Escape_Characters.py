f15fe4e20749b3757726605d1334638a20d0f202
Umer Dawood <umerdawoodahmaddawood@gmail.com>
2026-06-16 21:39:27 +0500

    octal value

--- Python_Syntax/7) Python_Escape_Characters.py
+++ Python_Syntax/7) Python_Escape_Characters.py
@@ -42,3 +42,12 @@ print(txt)
 
 Text = "This will return the Backspace, such as Umer \bDawood."
 print(Text)
+
+
+#A backslash followed by three integers will result in a octal value:
+txt = "\110\145\154\154\157"
+print(txt) 
+
+
+Text = "\000\111\224\223"
+print(Text)
