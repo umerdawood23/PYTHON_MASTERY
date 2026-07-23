928f27ca80207142a4a8080269844587f281c78d
Hermes Backup <umerdawoodahmaddawood@gmail.com>
2026-07-23 21:16:51 +0500

    Python_Operators

---
+++ 13)Python_Operators/Python_Operators.py
@@ -0,0 +1,7 @@
+sum1 = 100 + 50      # 150 (100 + 50)
+sum2 = sum1 + 250    # 400 (150 + 250)
+sum3 = sum2 + sum2   # 800 (400 + 400)
+
+
+sum4 = sum1 + sum2 + sum3
+print("The sum of sum4 is:", sum4)
