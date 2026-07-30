60df7e562bec644a6aef49a95411d962eec5336a
Hermes Backup <umerdawoodahmaddawood@gmail.com>
2026-07-30 20:59:13 +0500

    Membership_Operator

---
+++ 13)Python_Operators/7)Membership_Operators.py
@@ -0,0 +1,11 @@
+l = [1, 2, 3, 4, 5]
+s = "Hello World"
+d = {1: "Geeks", 2: "for", 3: "geeks"}
+
+print(2 in l)
+print('O' in s)
+print(3 in d)
+
+
+"""NOT IN OPERATOR"""
+"""It is the opposite of the"""
