b63495b635e29e2a00c2d44718ad564089963668
Hermes Backup <umerdawoodahmaddawood@gmail.com>
2026-08-05 22:32:42 +0500

    Coding Solutions

--- 13)Python_Operators/9)Operator_Precedence.py
+++ 13)Python_Operators/9)Operator_Precedence.py
@@ -7,3 +7,16 @@ The calculation above reads 9 - 9 = 0
 
 
 print((19+11) - (3-30))
+
+
+# Create variables
+a = 15
+b = 4
+# Print modulus
+print(a%b)
+# Print floor division
+print(a//b)
+# Print power
+print(a**b)
+# Add 10 to a
+print(a += 10)
