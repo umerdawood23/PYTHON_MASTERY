f662d8e782df4d3b3bd97d196d60f2f0411e57f5
Hermes Backup <umerdawoodahmaddawood@gmail.com>
2026-08-11 13:24:45 +0500

    Change List Items

--- 14)Python_List/3)Change_List_Items.py
+++ 14)Python_List/3)Change_List_Items.py
@@ -5,3 +5,17 @@ print(thislist)
 MyList = ["Umer", "ALi", "Bakar", "Usman"]
 MyList[0] = "Raza"
 print(MyList)
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+
+thislist[1:3] = ["blackcurrant", "watermelon"]
+
+print(thislist)
+
+
+thislist[0:6] = ["A", "B", "C", "D", "E", "F"]
+print(thislist)
+print(len(thislist))
+thislist[-1:-4] = ["A", "B", "C"]
+print(thislist)
+print(len(thislist))
