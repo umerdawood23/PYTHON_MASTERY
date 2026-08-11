7516e5041214c8c18f20bf92eaba85fddc4ffe31
Hermes Backup <umerdawoodahmaddawood@gmail.com>
2026-08-11 16:43:07 +0500

    Delete Method()

--- 14)Python_List/4)Remove_List_Items.py
+++ 14)Python_List/4)Remove_List_Items.py
@@ -1,3 +1,16 @@
 MyList = ["Apple", "Banana", "Kiwi"]
 MyList.remove("Apple")
 print(MyList)
+
+"""POP METHOD()"""
+MyList.pop(0)
+print(MyList)
+"""Pop Method Removes the Elment from a specificed Index.
+If not specified, then it removes the last element"""
+
+
+"""Del Method()"""
+"""It Also deletes the elements from the specified Index and also also delete the complete list as well"""
+MyList_Two = ["A", "B", "C", "D"]
+del MyList_Two()
+print(MyList_Two)
