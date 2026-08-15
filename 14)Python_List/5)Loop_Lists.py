3c9ca208e94f207c638b0b6c1b6bddcaf145410e
Hermes Backup <umerdawoodahmaddawood@gmail.com>
2026-08-15 20:44:14 +0500

    For Loop

--- 14)Python_List/5)Loop_Lists.py
+++ 14)Python_List/5)Loop_Lists.py
@@ -16,3 +16,12 @@ x = 0
 while x < 10:
       print(x)
       x = x + 1
+
+
+"""The For Loop"""
+for x in range(1,30):
+    print(x)
+
+
+""""The For keyword is used to create a Lopp that iterates over a list, sequence or numbers"""
+
