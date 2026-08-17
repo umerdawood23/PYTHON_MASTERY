1cf3e9df4dab0f17aa8da1fc6569225486500680
Hermes Backup <umerdawoodahmaddawood@gmail.com>
2026-08-17 21:55:18 +0500

    Sorting List

---
+++ 14)Python_List/7)Sort_Lists.py
@@ -0,0 +1,7 @@
+"""Question 1: Descending Sort
+You are tracking the high scores of a game. Given the list scores = [45, 92, 12, 78, 55], 
+write the code to sort the list numerically from highest to lowest, and then print the list."""
+
+thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
+thislist.sort()
+print(thislist)
