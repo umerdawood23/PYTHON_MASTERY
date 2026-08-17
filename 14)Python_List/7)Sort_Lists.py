e9838f18e0b15f26ee94ecf2067ad53af2483a15
Hermes Backup <umerdawoodahmaddawood@gmail.com>
2026-08-17 22:02:26 +0500

    STR LOWER

--- 14)Python_List/7)Sort_Lists.py
+++ 14)Python_List/7)Sort_Lists.py
@@ -10,3 +10,14 @@ print(thislist)
 list_score = [45, 92, 12, 78, 55]
 list_score.sort(reverse=True)
 print(list_score)
+
+
+"""Question 2: Case-Insensitive Sort
+You have a list of names entered by users with inconsistent capitalization: 
+names = ["Zack", "alice", "Bob", "charlie"]. 
+Write the code to sort this list alphabetically while ignoring the case sensitivity, so that "alice" correctly appears before "Bob". 
+Print the sorted list."""
+
+names = ["Zack", "alice", "Bob", "charlie"]
+names.sort(key = str.lower)
+print(names)
