e1130f662070fbabffde861c5f2aa25357eb6241
Hermes Backup <umerdawoodahmaddawood@gmail.com>
2026-08-19 20:41:14 +0500

    List method

--- 14)Python_List/8)Copy_Lists.py
+++ 14)Python_List/8)Copy_Lists.py
@@ -1,3 +1,13 @@
 thislist = ["apple", "banana", "cherry"]
 mylist = thislist.copy()
 print(mylist)
+
+"""Use the list() method"""
+
+thislist = ["apple", "banana", "cherry"]
+mylist = list(thislist)
+print(mylist)
+
+
+
+
